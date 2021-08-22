bc07948bec533efb18755c35c66ce12f40621054
halshorbagy <Halshorbagy@2p.com.sa>
2021-08-22 02:02:38 +0200

    layout

--- mdafen/src/app/app.module.ts
+++ mdafen/src/app/app.module.ts
@@ -7,7 +7,12 @@ import { SwiperModule } from 'swiper/angular';
 import {HomeComponent} from './components/home/home.component';
 import {HeaderComponent} from './components/header/header.component';
 import { SliderComponentComponent } from './components/slider-component/slider-component.component';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent }
@@ -16,13 +21,20 @@ const routes: Routes = [
   declarations: [
     AppComponent,
     HeaderComponent,
+    HomeComponent,
     SliderComponentComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),
     SwiperModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatSidenavModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
